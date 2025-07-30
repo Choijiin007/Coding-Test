c79957f7ac8a42d16800f72fb7bbd2071fbf51b9
Choijiin007 <cji2025@naver.com>
2025-07-30 12:47:08 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().split(" ");
+
+const year = Number(input[0]);
+
+if(year % 4 ===0 && !(year % 100 ===0) || year % 400===0){
+    console.log("1");
+}
+else {
+    console.log("0");
+}
